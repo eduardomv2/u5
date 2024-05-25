60ad4d56c3ad4630341f25932529809563cfe07d
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-05-25 12:07:19 -0600

    Agregar archivos de proyecto.

---
+++ u5/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace u5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.txtRutaCarpeta = new System.Windows.Forms.TextBox();
+            this.lblTamanoTotal = new System.Windows.Forms.Label();
+            this.txtRutaArchivo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(704, 329);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(413, 13);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(298, 28);
+            this.btnAnalizar.TabIndex = 1;
+            this.btnAnalizar.Text = "Analizar tamaños de los archivos en la Ruta Seleccionada";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // txtRutaCarpeta
+            // 
+            this.txtRutaCarpeta.Location = new System.Drawing.Point(92, 17);
+            this.txtRutaCarpeta.Name = "txtRutaCarpeta";
+            this.txtRutaCarpeta.Size = new System.Drawing.Size(315, 20);
+            this.txtRutaCarpeta.TabIndex = 2;
+            // 
+            // lblTamanoTotal
+            // 
+            this.lblTamanoTotal.AutoSize = true;
+            this.lblTamanoTotal.Location = new System.Drawing.Point(30, 21);
+            this.lblTamanoTotal.Name = "lblTamanoTotal";
+            this.lblTamanoTotal.Size = new System.Drawing.Size(35, 13);
+            this.lblTamanoTotal.TabIndex = 3;
+            this.lblTamanoTotal.Text = "label1";
+            // 
+            // txtRutaArchivo
+            // 
+            this.txtRutaArchivo.Location = new System.Drawing.Point(92, 54);
+            this.txtRutaArchivo.Name = "txtRutaArchivo";
+            this.txtRutaArchivo.Size = new System.Drawing.Size(315, 20);
+            this.txtRutaArchivo.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Leer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Poner Indices";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(563, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Leer el Indice Seleccionado";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtRutaArchivo);
+            this.Controls.Add(this.lblTamanoTotal);
+            this.Controls.Add(this.txtRutaCarpeta);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.TextBox txtRutaCarpeta;
+        private System.Windows.Forms.Label lblTamanoTotal;
+        private System.Windows.Forms.TextBox txtRutaArchivo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ u5/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace u5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Agregar columnas al DataGridView
+            dataGridView1.Columns.Add("NombreArchivo", "Nombre");
+            dataGridView1.Columns.Add("TamanoArchivo", "Tamaño");
+            dataGridView1.Columns.Add("FechaModificacion", "Fecha de Modificación");
+            dataGridView1.Columns.Add("TipoArchivo", "Tipo");
+
+            // Configurar la propiedad AutoSizeColumnsMode del DataGridView
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            string rutaCarpeta = txtRutaCarpeta.Text;
+
+            if (Directory.Exists(rutaCarpeta))
+            {
+                DirectoryInfo directorio = new DirectoryInfo(rutaCarpeta);
+
+                // Limpiar DataGridView antes de mostrar nuevos resultados
+                dataGridView1.Rows.Clear();
+
+                // Diccionarios para estadísticas
+                Dictionary<string, int> tipoArchivoConteo = new Dictionary<string, int>();
+                Dictionary<string, long> tipoArchivoTamano = new Dictionary<string, long>();
+                HashSet<string> archivosDuplicados = new HashSet<string>();
+
+                // Obtener archivos y subcarpetas
+                FileInfo[] archivos = directorio.GetFiles("*", SearchOption.AllDirectories);
+
+                // Calcular el tamaño total de la carpeta
+                long tamanoTotal = CalcularTamanoCarpeta(directorio);
+
+                // Mostrar el tamaño total en MB o GB, dependiendo de su magnitud
+                lblTamanoTotal.Text = FormatearTamano(tamanoTotal);
+
+                // Análisis de archivos
+                foreach (var archivo in archivos)
+                {
+                    string extension = archivo.Extension.ToLower();
+                    string nombreConTamano = archivo.Name + archivo.Length;
+
+                    // Contar tipos de archivos
+                    if (tipoArchivoConteo.ContainsKey(extension))
+                    {
+                        tipoArchivoConteo[extension]++;
+                        tipoArchivoTamano[extension] += archivo.Length;
+                    }
+                    else
+                    {
+                        tipoArchivoConteo[extension] = 1;
+                        tipoArchivoTamano[extension] = archivo.Length;
+                    }
+
+                    // Detectar archivos duplicados
+                    if (archivosDuplicados.Contains(nombreConTamano))
+                    {
+                        // Marcar como duplicado
+                        dataGridView1.Rows.Add(archivo.Name, FormatearTamano(archivo.Length), archivo.LastWriteTime, extension + " (Duplicado)");
+                    }
+                    else
+                    {
+                        archivosDuplicados.Add(nombreConTamano);
+                        dataGridView1.Rows.Add(archivo.Name, FormatearTamano(archivo.Length), archivo.LastWriteTime, extension);
+                    }
+                }
+
+                // Mostrar estadísticas
+                MostrarEstadisticas(tipoArchivoConteo, tipoArchivoTamano);
+            }
+            else
+            {
+                MessageBox.Show("La ruta especificada no es válida.");
+            }
+        }
+
+        // Método para calcular el tamaño total de la carpeta y sus subcarpetas
+        private long CalcularTamanoCarpeta(DirectoryInfo carpeta)
+        {
+            long tamanoTotal = 0;
+
+            // Calcular el tamaño de los archivos en la carpeta actual
+            foreach (var archivo in carpeta.GetFiles())
+            {
+                tamanoTotal += archivo.Length;
+            }
+
+            // Calcular el tamaño de las subcarpetas de manera recursiva
+            foreach (var subCarpeta in carpeta.GetDirectories())
+            {
+                tamanoTotal += CalcularTamanoCarpeta(subCarpeta);
+            }
+
+            return tamanoTotal;
+        }
+
+        // Método para formatear el tamaño en bytes a MB o GB, según su magnitud
+        private string FormatearTamano(long bytes)
+        {
+            const long kilobyte = 1024;
+            const long megabyte = kilobyte * 1024;
+            const long gigabyte = megabyte * 1024;
+
+            if (bytes >= gigabyte)
+            {
+                return $"{(double)bytes / gigabyte:0.##} GB";
+            }
+            else if (bytes >= megabyte)
+            {
+                return $"{(double)bytes / megabyte:0.##} MB";
+            }
+            else if (bytes >= kilobyte)
+            {
+                return $"{(double)bytes / kilobyte:0.##} KB";
+            }
+            else
+            {
+                return $"{bytes} bytes";
+            }
+        }
+
+        // Método para mostrar estadísticas en un MessageBox o en un control adicional
+        private void MostrarEstadisticas(Dictionary<string, int> tipoArchivoConteo, Dictionary<string, long> tipoArchivoTamano)
+        {
+            string estadisticas = "Estadísticas de tipos de archivos:\n\n";
+
+            foreach (var tipo in tipoArchivoConteo.Keys)
+            {
+                estadisticas += $"{tipo}: {tipoArchivoConteo[tipo]} archivos, {FormatearTamano(tipoArchivoTamano[tipo])} en total\n";
+            }
+
+            MessageBox.Show(estadisticas, "Estadísticas de archivos");
+        }
+
+        // Métodos adicionales para manejar diferentes tipos de organización de archivos
+
+        // Leer un archivo secuencial línea por línea
+        private void LeerArchivoSecuencial(string rutaArchivo)
+        {
+            if (File.Exists(rutaArchivo))
+            {
+                using (StreamReader sr = new StreamReader(rutaArchivo))
+                {
+                    string contenido = sr.ReadToEnd();
+                    MessageBox.Show("Contenido del archivo:\n" + contenido);
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo especificado no existe.");
+            }
+        }
+        // Método para crear un índice secuencial y mostrar el contenido del archivo
+        private void CrearIndiceSecuencial(string rutaArchivo)
+        {
+            if (File.Exists(rutaArchivo))
+            {
+                using (StreamReader sr = new StreamReader(rutaArchivo))
+                {
+                    string linea;
+                    int numeroLinea = 1;
+                    StringBuilder contenido = new StringBuilder();
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        contenido.AppendLine($"Línea {numeroLinea}: {linea}");
+                        numeroLinea++;
+                    }
+                    MessageBox.Show(contenido.ToString(), "Contenido del archivo");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo especificado no existe.");
+            }
+        }
+
+        // Método para leer una línea específica del archivo
+        private void LeerLineaEspecifica(string rutaArchivo)
+        {
+            if (File.Exists(rutaArchivo))
+            {
+                string numeroLineaStr = Microsoft.VisualBasic.Interaction.InputBox("Ingrese el número de línea a leer:", "Lectura de línea", "1");
+                if (int.TryParse(numeroLineaStr, out int numeroLinea))
+                {
+                    using (StreamReader sr = new StreamReader(rutaArchivo))
+                    {
+                        string linea;
+                        int contadorLinea = 1;
+                        while ((linea = sr.ReadLine()) != null)
+                        {
+                            if (contadorLinea == numeroLinea)
+                            {
+                                MessageBox.Show($"Contenido de la línea {numeroLinea}: {linea}", "Línea específica");
+                                return;
+                            }
+                            contadorLinea++;
+                        }
+                        MessageBox.Show($"El archivo no tiene {numeroLinea} líneas.", "Línea no encontrada");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingrese un número válido.", "Error de entrada");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo especificado no existe.");
+            }
+        }
+
+        // Crear un índice simple para un archivo secuencial indexado
+        private Dictionary<int, long> CrearIndiceArchivo(string rutaArchivo)
+        {
+            var indice = new Dictionary<int, long>();
+            if (File.Exists(rutaArchivo))
+            {
+                using (StreamReader sr = new StreamReader(rutaArchivo))
+                {
+                    long posicion = 0;
+                    int numeroLinea = 0;
+                    string linea;
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        indice[numeroLinea] = posicion;
+                        posicion = sr.BaseStream.Position;
+                        numeroLinea++;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo especificado no existe.");
+            }
+            return indice;
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string rutaArchivo = txtRutaArchivo.Text;
+            LeerArchivoSecuencial(rutaArchivo);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string rutaArchivo = txtRutaArchivo.Text;
+            CrearIndiceSecuencial(rutaArchivo);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string rutaArchivo = txtRutaArchivo.Text;
+            LeerLineaEspecifica(rutaArchivo);
+        }
+    }
+}
+
